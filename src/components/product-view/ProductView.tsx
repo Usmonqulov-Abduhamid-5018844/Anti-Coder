c0da2b8d7a283388f50145ff0c8298afb3f5ef4b
Yagshigeldi <yagshigeldi5425@gmail.com>
2025-08-08 12:50:40 +0500

    Product va Brand componentlari ui yaratildi, media hali qilingani yoq

--- src/components/product-view/ProductView.tsx
+++ src/components/product-view/ProductView.tsx
@@ -1,116 +1,187 @@
 import React from "react";
-import ProductImage from "../../assets/ProductImage.png";
-import ProductVector from "../../assets/ProductVector.svg";
-import ProductBasket from "../../assets/ProductBasket.svg";
+import ProductImage from "../../assets/product-images/ProductImage.png";
+import ProductVector from "../../assets/product-images/ProductVector.svg";
+import ProductBasket from "../../assets/product-images/ProductBasket.svg";
 
 const ProductView = () => {
   return (
-    <section className="container py-10">
-      <div className="flex justify-between items-center mb-8">
-        <h2 className="text-xl font-bold text-[#454545]">Популярные товары</h2>
-        <button className="flex items-center gap-2 text-gray-600 hover:text-black">
-          Все товары
-          <img src={ProductVector} alt="" />
-        </button>
-      </div>
+    <section>
+      <div className="container mt-[120px]">
+        <div className="flex justify-between items-center gap-4">
+          <h2 className="text-[#454545] text-[28px] font-bold">
+            Популярные товары
+          </h2>
 
-      <div className="flex gap-6 mb-6">
-        <div className="p-4 flex flex-col items-center relative">
-          <img src={ProductImage} alt="" className="w-32 h-32 object-contain mb-4" />
-          <h3 className="text-center text-sm font-medium mb-1">Встраиваемый <br /> светильник Novotech</h3>
-          <div className="flex flex-col items-center mb-2">
-            <span className="line-through text-gray-400 text-xs">7 000₽</span>
-            <span className="text-lg font-semibold">6 399₽</span>
+          <div className="w-[220px] h-[50px] flex border rounded-[100px] justify-center items-center gap-[23px] text-[#454545] text-[16px] font-medium cursor-pointer">
+            <div>
+              <p>Все товары</p>
+            </div>
+            <div>
+              <img src={ProductVector} alt="" />
+            </div>
           </div>
-          <button className="absolute bottom-4 right-4 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
-            <img src={ProductBasket} alt="" />
-          </button>
         </div>
 
-        <div className="p-4 flex flex-col items-center relative">
-          <img src={ProductImage} alt="" className="w-32 h-32 object-contain mb-4" />
-          <h3 className="text-center text-sm font-medium mb-1">Встраиваемый <br /> светильник Novotech</h3>
-          <div className="flex flex-col items-center mb-2">
-            <span className="line-through text-gray-400 text-xs">7 000₽</span>
-            <span className="text-lg font-semibold">6 399₽</span>
+        <div className="mt-[40px] grid grid-cols-4 gap-[24px]">
+          <div className="flex flex-col justify-center items-start px-4 py-6">
+            <img
+              src={ProductImage}
+              alt=""
+              className="w-full max-w-[160px] h-auto object-contain mx-auto mb-6"
+            />
+            <p className="text-[16px] text-[#454545] font-medium mb-4">
+              Встраиваемый <br /> светильник Novotech
+            </p>
+            <div className="flex justify-between items-end w-full">
+              <div className="flex flex-col">
+                <p className="line-through text-[#9F9F9F] text-sm">7 000₽</p>
+                <p className="text-[#454545] font-bold text-[18px]">6 399₽</p>
+              </div>
+              <button className="bg-[#454545] rounded-full px-5 py-2 cursor-pointer">
+                <img src={ProductBasket} alt="Cart" />
+              </button>
+            </div>
           </div>
-          <button className="absolute bottom-4 right-4 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
-            <img src={ProductBasket} alt="" />
-          </button>
-        </div>
 
-        <div className="p-4 flex flex-col items-center relative">
-          <img src={ProductImage} alt="" className="w-32 h-32 object-contain mb-4" />
-          <h3 className="text-center text-sm font-medium mb-1">Встраиваемый <br /> светильник Novotech</h3>
-          <div className="flex flex-col items-center mb-2">
-            <span className="line-through text-gray-400 text-xs">7 000₽</span>
-            <span className="text-lg font-semibold">6 399₽</span>
+          <div className="flex flex-col justify-center items-start px-4 py-6">
+            <img
+              src={ProductImage}
+              alt=""
+              className="w-full max-w-[160px] h-auto object-contain mx-auto mb-6"
+            />
+            <p className="text-[16px] text-[#454545] font-medium mb-4">
+              Встраиваемый <br /> светильник Novotech
+            </p>
+            <div className="flex justify-between items-end w-full">
+              <div className="flex flex-col">
+                <p className="line-through text-[#9F9F9F] text-sm">7 000₽</p>
+                <p className="text-[#454545] font-bold text-[18px]">6 399₽</p>
+              </div>
+              <button className="bg-[#454545] rounded-full px-5 py-2 cursor-pointer">
+                <img src={ProductBasket} alt="Cart" />
+              </button>
+            </div>
           </div>
-          <button className="absolute bottom-4 right-4 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
-            <img src={ProductBasket} alt="" />
-          </button>
-        </div>
 
-        <div className="p-4 flex flex-col items-center relative">
-          <img src={ProductImage} alt="" className="w-32 h-32 object-contain mb-4" />
-          <h3 className="text-center text-sm font-medium mb-1">Встраиваемый <br /> светильник Novotech</h3>
-          <div className="flex flex-col items-center mb-2">
-            <span className="line-through text-gray-400 text-xs">7 000₽</span>
-            <span className="text-lg font-semibold">6 399₽</span>
+          <div className="flex flex-col justify-center items-start px-4 py-6">
+            <img
+              src={ProductImage}
+              alt=""
+              className="w-full max-w-[160px] h-auto object-contain mx-auto mb-6"
+            />
+            <p className="text-[16px] text-[#454545] font-medium mb-4">
+              Встраиваемый <br /> светильник Novotech
+            </p>
+            <div className="flex justify-between items-end w-full">
+              <div className="flex flex-col">
+                <p className="line-through text-[#9F9F9F] text-sm">7 000₽</p>
+                <p className="text-[#454545] font-bold text-[18px]">6 399₽</p>
+              </div>
+              <button className="bg-[#454545] rounded-full px-5 py-2 cursor-pointer">
+                <img src={ProductBasket} alt="Cart" />
+              </button>
+            </div>
           </div>
-          <button className="absolute bottom-4 right-4 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
-            <img src={ProductBasket} alt="" />
-          </button>
-        </div>
-      </div>
 
-      <div className="flex gap-6">
-        <div className="p-4 flex flex-col items-center relative">
-          <img src={ProductImage} alt="" className="w-32 h-32 object-contain mb-4" />
-          <h3 className="text-center text-sm font-medium mb-1">Встраиваемый <br /> светильник Novotech</h3>
-          <div className="flex flex-col items-center mb-2">
-            <span className="line-through text-gray-400 text-xs">7 000₽</span>
-            <span className="text-lg font-semibold">6 399₽</span>
+          <div className="flex flex-col justify-center items-start px-4 py-6">
+            <img
+              src={ProductImage}
+              alt=""
+              className="w-full max-w-[160px] h-auto object-contain mx-auto mb-6"
+            />
+            <p className="text-[16px] text-[#454545] font-medium mb-4">
+              Встраиваемый <br /> светильник Novotech
+            </p>
+            <div className="flex justify-between items-end w-full">
+              <div className="flex flex-col">
+                <p className="line-through text-[#9F9F9F] text-sm">7 000₽</p>
+                <p className="text-[#454545] font-bold text-[18px]">6 399₽</p>
+              </div>
+              <button className="bg-[#454545] rounded-full px-5 py-2 cursor-pointer">
+                <img src={ProductBasket} alt="Cart" />
+              </button>
+            </div>
           </div>
-          <button className="absolute bottom-4 right-4 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
-            <img src={ProductBasket} alt="" />
-          </button>
-        </div>
 
-        <div className="p-4 flex flex-col items-center relative">
-          <img src={ProductImage} alt="" className="w-32 h-32 object-contain mb-4" />
-          <h3 className="text-center text-sm font-medium mb-1">Встраиваемый <br /> светильник Novotech</h3>
-          <div className="flex flex-col items-center mb-2">
-            <span className="line-through text-gray-400 text-xs">7 000₽</span>
-            <span className="text-lg font-semibold">6 399₽</span>
+          <div className="flex flex-col justify-center items-start px-4 py-6">
+            <img
+              src={ProductImage}
+              alt=""
+              className="w-full max-w-[160px] h-auto object-contain mx-auto mb-6"
+            />
+            <p className="text-[16px] text-[#454545] font-medium mb-4">
+              Встраиваемый <br /> светильник Novotech
+            </p>
+            <div className="flex justify-between items-end w-full">
+              <div className="flex flex-col">
+                <p className="line-through text-[#9F9F9F] text-sm">7 000₽</p>
+                <p className="text-[#454545] font-bold text-[18px]">6 399₽</p>
+              </div>
+              <button className="bg-[#454545] rounded-full px-5 py-2 cursor-pointer">
+                <img src={ProductBasket} alt="Cart" />
+              </button>
+            </div>
           </div>
-          <button className="absolute bottom-4 right-4 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
-            <img src={ProductBasket} alt="" />
-          </button>
-        </div>
 
-        <div className="p-4 flex flex-col items-center relative">
-          <img src={ProductImage} alt="" className="w-32 h-32 object-contain mb-4" />
-          <h3 className="text-center text-sm font-medium mb-1">Встраиваемый <br /> светильник Novotech</h3>
-          <div className="flex flex-col items-center mb-2">
-            <span className="line-through text-gray-400 text-xs">7 000₽</span>
-            <span className="text-lg font-semibold">6 399₽</span>
+          <div className="flex flex-col justify-center items-start px-4 py-6">
+            <img
+              src={ProductImage}
+              alt=""
+              className="w-full max-w-[160px] h-auto object-contain mx-auto mb-6"
+            />
+            <p className="text-[16px] text-[#454545] font-medium mb-4">
+              Встраиваемый <br /> светильник Novotech
+            </p>
+            <div className="flex justify-between items-end w-full">
+              <div className="flex flex-col">
+                <p className="line-through text-[#9F9F9F] text-sm">7 000₽</p>
+                <p className="text-[#454545] font-bold text-[18px]">6 399₽</p>
+              </div>
+              <button className="bg-[#454545] rounded-full px-5 py-2 cursor-pointer">
+                <img src={ProductBasket} alt="Cart" />
+              </button>
+            </div>
           </div>
-          <button className="absolute bottom-4 right-4 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
-            <img src={ProductBasket} alt="" />
-          </button>
-        </div>
 
-        <div className="p-4 flex flex-col items-center relative">
-          <img src={ProductImage} alt="" className="w-32 h-32 object-contain mb-4" />
-          <h3 className="text-center text-sm font-medium mb-1">Встраиваемый <br /> светильник Novotech</h3>
-          <div className="flex flex-col items-center mb-2">
-            <span className="line-through text-gray-400 text-xs">7 000₽</span>
-            <span className="text-lg font-semibold">6 399₽</span>
+          <div className="flex flex-col justify-center items-start px-4 py-6">
+            <img
+              src={ProductImage}
+              alt=""
+              className="w-full max-w-[160px] h-auto object-contain mx-auto mb-6"
+            />
+            <p className="text-[16px] text-[#454545] font-medium mb-4">
+              Встраиваемый <br /> светильник Novotech
+            </p>
+            <div className="flex justify-between items-end w-full">
+              <div className="flex flex-col">
+                <p className="line-through text-[#9F9F9F] text-sm">7 000₽</p>
+                <p className="text-[#454545] font-bold text-[18px]">6 399₽</p>
+              </div>
+              <button className="bg-[#454545] rounded-full px-5 py-2 cursor-pointer">
+                <img src={ProductBasket} alt="Cart" />
+              </button>
+            </div>
           </div>
-          <button className="absolute bottom-4 right-4 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
-            <img src={ProductBasket} alt="" />
-          </button>
+
+          <div className="flex flex-col justify-center items-start px-4 py-6">
+            <img
+              src={ProductImage}
+              alt=""
+              className="w-full max-w-[160px] h-auto object-contain mx-auto mb-6"
+            />
+            <p className="text-[16px] text-[#454545] font-medium mb-4">
+              Встраиваемый <br /> светильник Novotech
+            </p>
+            <div className="flex justify-between items-end w-full">
+              <div className="flex flex-col">
+                <p className="line-through text-[#9F9F9F] text-sm">7 000₽</p>
+                <p className="text-[#454545] font-bold text-[18px]">6 399₽</p>
+              </div>
+              <button className="bg-[#454545] rounded-full px-5 py-2 cursor-pointer">
+                <img src={ProductBasket} alt="Cart" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
