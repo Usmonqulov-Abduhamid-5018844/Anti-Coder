223a2977e5e2f29a82ba0fb0998eac91a5320eea
⚜️ <asadbekmamajonov@gaaa.local>
2025-08-08 23:40:29 +0500

    some fixes in Detail page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ const MainRouters = () => {
           children: [
             { index: true, element: <Home /> },
             {path: "/", element: <Home/>},
-            { path: "product/:id", element: <ProductDetail /> },
+            { path: "product", element: <ProductDetail /> },
 
 
             {path: "kompany", element:<Kompany/> },
